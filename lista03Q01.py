470f689d5b96e65502f21c94a8160a3b466e82a3
malvesalessandro <43225857+malvesalessandro@users.noreply.github.com>
2018-09-13 17:32:09 -0300

    Update lista03Q01.py

--- lista03Q01.py
+++ lista03Q01.py
@@ -1,8 +1,8 @@
-num = input("digite um numero?")
+num = int(input("digite um numero?"))
 while num > 1:
     if num % 3 == 0:
         num = num/3
-        print (num)
+        print (int(num))
     else:
         if num % 3 == 1:
             num = num - 1
